a5b8aca983f80001f5a62d65fb5a9782cd1d8702
Liam Scholte <scholte.l@northeastern.edu>
2022-03-27 23:22:10 -0700

    Removes old project files

--- ChessArView/Macros.h
+++
@@ -1,8 +0,0 @@
-// Author:	Liam Scholte
-// Created:	3/6/2022 1:21:21 PM
-// Nothing important happening here.
-// Just some necessary infrastructure
-
-#pragma once
-
-#define EXPORT _declspec(dllexport)
