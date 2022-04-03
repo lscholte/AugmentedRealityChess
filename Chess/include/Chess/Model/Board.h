a48f2f51001757bd0bcd64bbb2120497052ed732
Liam Scholte <scholte.l@northeastern.edu>
2022-04-03 01:36:40 -0700

    Adds class documentation for the Board class

--- Chess/include/Chess/Model/Board.h
+++ Chess/include/Chess/Model/Board.h
@@ -17,6 +17,9 @@ namespace Model
 	struct Position;
 	struct Size;
 
+	/// <summary>
+	/// Represents an 8x8 chessboard that can hold various chess pieces.
+	/// </summary>
 	class EXPORT Board
 	{
 	public:
@@ -35,9 +38,23 @@ namespace Model
 
 		virtual ~Board();
 
+		/// <summary>
+		/// Gets the dimensions of the board.
+		/// </summary>
+		/// <returns>The dimensions of the board</returns>
 		Size getSize() const;
 
+		/// <summary>
+		/// Gets all of the pieces on the board.
+		/// </summary>
+		/// <returns>All of the pieces on the board</returns>
 		std::unordered_set<std::shared_ptr<Piece>> const& getPieces() const;
+
+		/// <summary>
+		/// Gets all of the pieces of the specified color on the board.
+		/// </summary>
+		/// <param name="isWhite">Whether to get white or black pieces</param>
+		/// <returns>All of the pieces of the specified color on the board</returns>
 		std::unordered_set<std::shared_ptr<Piece>> const& getPieces(bool isWhite) const;
 
 		/// <summary>
@@ -47,10 +64,35 @@ namespace Model
 		/// <returns>The piece at the position or nullptr if no piece exists</returns>
 		std::shared_ptr<Piece> getPiece(Position position) const;
 
+		/// <summary>
+		/// Removes the specified piece from the board.
+		/// </summary>
+		/// <param name="pPiece">The piece to remove</param>
+		/// <returns>True if the piece was removed, false otherwise</returns>
 		bool removePiece(std::shared_ptr<Piece> pPiece);
 
+		/// <summary>
+		/// Determines if a specified position is on the board.
+		/// </summary>
+		/// <param name="position">The position to check</param>
+		/// <returns>True if the position is on the board, false otherwise</returns>
 		bool isPositionOnBoard(Position position) const;
+
+		/// <summary>
+		/// Determines if it is possible to move a piece of a particular color to a particular position.
+		/// A position is considered possible if it is on the board and there is no piece of the
+		/// same color in the position already.
+		/// </summary>
+		/// <param name="isWhite">Whether the piece that is being checked is white or black</param>
+		/// <param name="position">The position to check</param>
+		/// <returns>True if a piece could possibly be moved into the specified position, false otherwise</returns>
 		bool isPositionPossible(bool isWhite, Position position) const;
+
+		/// <summary>
+		/// Determines if the king for a particular color is in check given the current state of the board.
+		/// </summary>
+		/// <param name="isWhite">Whether the king that is being checked is white or black</param>
+		/// <returns>True if the king is in check, false otherwise</returns>
 		bool isKingInCheck(bool isWhite) const;
 
 
