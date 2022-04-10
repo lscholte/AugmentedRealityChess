2c63a9f2dd0e8fda5fd718c78aec03a7bb4f74ec
Liam Scholte <lscholte@uvic.ca>
2022-04-09 17:03:46 -0700

    Adds class documentation for PieceFactory

--- Chess/include/Chess/Model/PieceFactory.h
+++ Chess/include/Chess/Model/PieceFactory.h
@@ -14,12 +14,26 @@ namespace Model
 	enum class PieceType;
 	struct Position;
 
+	/// <summary>
+	/// A factory class that creates chess pieces.
+	/// </summary>
 	class PieceFactory
 	{
 	public:
+
+		/// <summary>
+		/// Constructs a factory for creating pieces.
+		/// </summary>
+		/// <param name="isWhite">Whether this factory creates white or black pieces</param>
 		PieceFactory(bool isWhite);
 		virtual ~PieceFactory();
 
+		/// <summary>
+		/// Creates a chess piece of a given type at a position on the board
+		/// </summary>
+		/// <param name="type">The type of piece to create</param>
+		/// <param name="position">The position of the piece on the board</param>
+		/// <returns>The created chess piece</returns>
 		std::shared_ptr<Piece> create(PieceType type, Position position) const;
 
 	private:
