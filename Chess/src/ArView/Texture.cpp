b7db43f79875cf94005fe9a5ce25c88faeed4322
Liam Scholte <scholte.l@northeastern.edu>
2022-04-12 00:44:46 -0700

    Hacks together displaying an actual chessboard model

---
+++ Chess/src/ArView/Texture.cpp
@@ -0,0 +1,22 @@
+//// Author:	Liam Scholte
+//// Created:	4/11/2022 11:06:27 PM
+//// This file contains the implementations for Texture
+//// See Texture.h for documentation
+//
+//#include "Texture.h"
+//
+//namespace Chess
+//{
+//namespace ArView
+//{
+//	struct Texture
+//	{
+//		unsigned int id;
+//		std::string type;
+//		std::string path;
+//
+//		Texture::~Texture() = default;
+//	};
+//}
+//}
+//
--- Chess/src/ArView/shaders/ObjectShader.frag
+++ Chess/src/ArView/shaders/ObjectShader.frag
@@ -8,7 +8,7 @@ in vec2 FragmentTextureCoord;
 out vec4 FragColor;
 
 uniform bool HasImage;
-uniform sampler2D Image;
+uniform sampler2D texture_diffuse1;
 
 uniform vec3 CameraWorldPosition;
 
@@ -17,7 +17,7 @@ void main()
 	vec4 color;
 	if (HasImage)
 	{
-		color = texture(Image, FragmentTextureCoord);
+		color = texture(texture_diffuse1, FragmentTextureCoord);
 	}
 	else
 	{
