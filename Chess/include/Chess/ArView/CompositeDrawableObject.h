d85a1dcb1be6b6a50fd946bbdf013873d1241d94
Liam Scholte <scholte.l@northeastern.edu>
2022-04-12 21:56:00 -0700

    Adds class documentation for CompositeDrawableObject

--- Chess/include/Chess/ArView/CompositeDrawableObject.h
+++ Chess/include/Chess/ArView/CompositeDrawableObject.h
@@ -13,11 +13,20 @@ namespace Chess
 {
 namespace ArView
 {
+	/// <summary>
+	/// A DrawableObject that is composed of other DrawableObjects.
+	/// </summary>
 	class CompositeDrawableObject
 		: public DrawableObject
 	{
 	public:
+
+		/// <summary>
+		/// Constructs a CompositeDrawableObject from a vector of DrawableObjects.
+		/// </summary>
+		/// <param name="drawableObjects">The objects that should be drawn when this object is drawn</param>
 		CompositeDrawableObject(std::vector<std::shared_ptr<DrawableObject>> const& drawableObjects);
+
 		virtual ~CompositeDrawableObject();
 
 		virtual void draw() const override;
