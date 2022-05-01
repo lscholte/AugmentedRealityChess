f869b4f7ecda2e2e78ce70a93befd1ac19663a73
Liam Scholte <lscholte@uvic.ca>
2022-04-30 22:16:10 -0700

    Adds documentation for the Controller class

--- Chess/include/Chess/Controller/Controller.h
+++ Chess/include/Chess/Controller/Controller.h
@@ -13,17 +13,45 @@ namespace Chess
 {
 namespace Controller
 {
+	/// <summary>
+	/// Handles inputs to a game of chess
+	/// </summary>
 	class EXPORT Controller
 	{
 	public:
+		/// <summary>
+		/// Constructs a controller for a game of chess.
+		/// </summary>
 		Controller();
+
 		virtual ~Controller();
 
+		/// <summary>
+		/// Gets the game of chess
+		/// </summary>
+		/// <returns>The game of chess</returns>
 		Model::Game const& getGame() const;
 
+		/// <summary>
+		/// Gets the piece at the selected position.
+		/// </summary>
+		/// <returns>
+		/// The piece at the selected position or an empty pointer
+		/// if no piece exists at that position or there is no selected position
+		/// </returns>
 		std::shared_ptr<Model::Piece> getSelectedPiece() const;
 
+		/// <summary>
+		/// Selects the specified position. This may result in a piece
+		/// being moved if the selected position is a legal move for
+		/// a piece at an already selected position.
+		/// </summary>
+		/// <param name="position">The position to select</param>
 		void selectPosition(Model::Position position);
+
+		/// <summary>
+		/// Unselects a position.
+		/// </summary>
 		void unselectPosition();
 
 	private:
